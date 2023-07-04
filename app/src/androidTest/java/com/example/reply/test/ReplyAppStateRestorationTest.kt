8fe1fd1edbacfa9cc16614c849d292e4b14d673f
Glenn Halls <gjhalls@gmail.com>
2023-07-04 20:35:30 +1000

    Test: state change

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -0,0 +1,91 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.StateRestorationTester
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.reply.data.local.LocalEmailsDataProvider
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+import com.example.reply.R
+
+class ReplyAppStateRestorationTest {
+
+    // Note: To access an empty activity, the code uses ComponentActivity instead of MainActivity
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactDevice_selectedEmailEmailRetained_afterConfigChange() {
+        // Setup compact window
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent { ReplyApp(WindowWidthSizeClass.Compact) }
+
+        // Ensure a third email is displayed
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        // Open page detail
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)
+        ).performClick()
+
+        // Verify that it shows the detailed screen for the correct email
+        composeTestRule.onNodeWithContentDescriptionForStringId(R.string.navigation_back)
+            .assertExists()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body))
+            .assertExists()
+
+        // Simulate a configuration change
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        // Verify that it STILL shows the detailed screen for the correct email
+        composeTestRule.onNodeWithContentDescriptionForStringId(R.string.navigation_back)
+            .assertExists()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body))
+            .assertExists()
+    }
+
+    // Run the same tests again, but with the expanded window size
+    @Test
+    fun expandedDevice_selectedEmailEmailRetained_afterConfigChange() {
+        // Setup compact window
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent { ReplyApp(WindowWidthSizeClass.Expanded) }
+
+        // Ensure a third email is displayed
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        // Open page detail
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)
+        ).performClick()
+
+        // Verify that it shows the detailed screen for the correct email
+        composeTestRule.onNodeWithContentDescriptionForStringId(R.string.navigation_back)
+            .assertExists()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body))
+            .assertExists()
+
+        // Simulate a configuration change
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        // Verify that it STILL shows the detailed screen for the correct email
+        composeTestRule.onNodeWithContentDescriptionForStringId(R.string.navigation_back)
+            .assertExists()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body))
+            .assertExists()
+    }
+
+}
